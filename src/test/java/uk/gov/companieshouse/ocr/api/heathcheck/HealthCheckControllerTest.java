fbdfa207883f2c7a2bd984b538619784a1e54f79
SteveHicksCH <shicks@companieshouse.gov.uk>
2020-12-02 16:36:49 +0000

    add heathcheck

---
+++ src/test/java/uk/gov/companieshouse/ocr/api/heathcheck/HealthCheckControllerTest.java
@@ -0,0 +1,34 @@
+package uk.gov.companieshouse.ocr.api.heathcheck;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import uk.gov.companieshouse.ocr.api.groups.TestType;
+
+@Tag(TestType.UNIT)
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HealthCheckController.class)
+public class HealthCheckControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+	@Test
+	public void validateIsHealthy() throws Exception {
+
+           mockMvc.perform(MockMvcRequestBuilders.get(HealthCheckController.HEATH_CHECK_PARTIAL_URL))
+		   .andExpect(status().isOk())
+		   .andExpect(content().string(containsString(HealthCheckController.HEATH_CHECK_MESSAGE)));
+	}
+}
