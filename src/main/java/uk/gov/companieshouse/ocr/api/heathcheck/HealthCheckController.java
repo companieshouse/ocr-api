6d29b97e896e08588b9c0704ba5ceba000fd818a
SteveHicksCH <shicks@companieshouse.gov.uk>
2020-12-02 17:32:36 +0000

    add logging when heath check is called

--- src/main/java/uk/gov/companieshouse/ocr/api/heathcheck/HealthCheckController.java
+++ src/main/java/uk/gov/companieshouse/ocr/api/heathcheck/HealthCheckController.java
@@ -5,6 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
+import uk.gov.companieshouse.ocr.api.OcrApiApplication;
 
 @RestController
 public class HealthCheckController {
@@ -12,9 +15,12 @@ public class HealthCheckController {
     public static final String HEATH_CHECK_PARTIAL_URL = "/healthcheck";
     public static final String HEATH_CHECK_MESSAGE = "ALIVE";
 
+    private static final Logger LOG = LoggerFactory.getLogger(OcrApiApplication.APPLICATION_NAME_SPACE);
 
     @GetMapping(HEATH_CHECK_PARTIAL_URL)
     public @ResponseBody ResponseEntity<String> heathCheck() {
+
+        LOG.info("Heath Check called");
         return new ResponseEntity<>(HEATH_CHECK_MESSAGE, HttpStatus.OK);
 
     }
