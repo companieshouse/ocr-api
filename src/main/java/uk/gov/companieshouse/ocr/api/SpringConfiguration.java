3a321b8547e673ce48bc75e74ad1a134b7897e68
JoshFen95 <jfennell@companieshouse.gov.uk>
2022-09-06 15:18:31 +0100

    Setup new env var
    Configure white list env which will contain a comma seperated string
    listing the names of allowed url hosts that the ocr-api will accept

--- src/main/java/uk/gov/companieshouse/ocr/api/SpringConfiguration.java
+++ src/main/java/uk/gov/companieshouse/ocr/api/SpringConfiguration.java
@@ -22,6 +22,9 @@ public class SpringConfiguration {
     @Value("${low.confidence.to.log}")
     private int lowConfidenceToLog;
 
+    @Value("${host.white.list.values}")
+    private String hostWhiteList;
+
     private static final Logger LOG = LoggerFactory.getLogger(OcrApiApplication.APPLICATION_NAME_SPACE);
 
     @Bean
@@ -39,6 +42,7 @@ public class SpringConfiguration {
         LOG.info("The value of ${ocr.queue.capacity} is          :     " + ocrQueueCapacity);
         LOG.info("The value of ${low.confidence.to.log} is       :     " + lowConfidenceToLog);
         LOG.info("The value of ${rest.client.timeout.seconds} is :     " + restClientTimeoutSeconds);
+        LOG.info("The value of ${host.white.list.values} is      :     " + hostWhiteList);
 
 		LOG.info("-------------------- End displaying spring application.properties  ----------------------------------");
 	}
@@ -50,4 +54,9 @@ public class SpringConfiguration {
     public int getLowConfidenceToLog() {
         return lowConfidenceToLog;
     }
+
+    public String getHostWhiteList() {
+      return hostWhiteList;
+    }
+
 }
