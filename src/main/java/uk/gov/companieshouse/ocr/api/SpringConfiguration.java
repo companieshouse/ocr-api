0ce8fdd7d2d479c7f4671c346ecb814ec48d5f25
JoshFen95 <jfennell@companieshouse.gov.uk>
2022-09-20 12:01:58 +0100

    Formatting
    - move final var above non final vars

--- src/main/java/uk/gov/companieshouse/ocr/api/SpringConfiguration.java
+++ src/main/java/uk/gov/companieshouse/ocr/api/SpringConfiguration.java
@@ -1,17 +1,15 @@
 package uk.gov.companieshouse.ocr.api;
 
+import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
-import javax.annotation.PostConstruct;
-import java.time.Duration;
 
 @Configuration
 public class SpringConfiguration {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OcrApiApplication.APPLICATION_NAME_SPACE);
 
     @Value("${ocr.queue.capacity}")
     private int ocrQueueCapacity;
@@ -24,8 +22,6 @@ public class SpringConfiguration {
 
     @Value("${host.white.list}")
     private String hostWhiteList;
-
-    private static final Logger LOG = LoggerFactory.getLogger(OcrApiApplication.APPLICATION_NAME_SPACE);
 
     @PostConstruct
 	private void displaySpringProperties() {
