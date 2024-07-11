e750e9ae7c7e13918f67ceece8ab3d268023f930
nawazish587 <nali@companieshouse.gov.uk>
2024-07-11 12:17:54 +0100

    Updated OCR-API from Java 11 to 21
     On branch feature/bi-13718-java-21-upgrade
     Changes to be committed:
    	modified:   pom.xml
    	modified:   src/main/java/uk/gov/companieshouse/ocr/api/logging/LoggingInterceptor.java

--- src/main/java/uk/gov/companieshouse/ocr/api/logging/LoggingInterceptor.java
+++ src/main/java/uk/gov/companieshouse/ocr/api/logging/LoggingInterceptor.java
@@ -1,7 +1,7 @@
 package uk.gov.companieshouse.ocr.api.logging;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
